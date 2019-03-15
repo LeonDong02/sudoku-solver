ea0e40d3e7a53d4dcdaa068439f467dfc3011b3a
Leon Dong <leon.d1043@gmail.com>
2019-03-15 18:11:06 -0400

    removed incorrect algorithm, planning on implementing crosshatching

--- GUI.py
+++ GUI.py
@@ -2,39 +2,7 @@ from tkinter import *
 
 
 def fillgrid():
-    def check(grid):
-        possible = [[[1, 2, 3, 4, 5, 6, 7, 8, 9] for _ in range(9)] for _ in range(9)]
-        for i in range(9):
-            for j in range(9):
-                if grid[i][j] != '_':
-                    possible[i][j] = []
-                else:
-                    for k in range(9):
-                        if grid[i][k] != '_' and int(grid[i][k]) in possible[i][j]:
-                            possible[i][j].remove(int(grid[i][k]))
-                        if grid[k][j] != '_' and int(grid[k][j]) in possible[i][j]:
-                            possible[i][j].remove(int(grid[k][j]))
-                    for k in range(3):
-                        for l in range(3):
-                            if grid[i - (i % 3) + k][j - (j % 3) + l] != '_' and int(
-                                    grid[i - (i % 3) + k][j - (j % 3) + l]) in possible[i][j]:
-                                possible[i][j].remove(int(grid[i - (i % 3) + k][j - (j % 3) + l]))
-        minlen = 9
-        ind = []
-        for i in range(9):
-            for j in range(9):
-                if 0 < len(possible[i][j]) < minlen:
-                    minlen = len(possible[i][j])
-                    ind = [i, j]
-        grid[ind[0]][ind[1]] = possible[ind[0]][ind[1]][0]
-        return grid
-
     grid = [[gr[i][j].get() for j in range(9)] for i in range(9)]
-    while any('_' in grid[i] for i in range(9)):
-        grid = check(grid)
-    for i in range(9):
-        for j in range(9):
-            gr[i][j].configure(text=grid[i][j])
 
 
 window = Tk()
@@ -49,7 +17,7 @@ title.pack()
 title.place(anchor='center', height=100, width=250, x=400, y=50)
 
 
-subtitle = Label(window, text='Enter numbers and underscores ("_") for blanks', font=('Arial', 12))
+subtitle = Label(window, text='Enter numbers and leave blanks blank', font=('Arial', 12))
 subtitle.pack()
 subtitle.place(anchor='center', height=50, width=350, x=400, y=100)
 
@@ -62,7 +30,7 @@ gr = [[] for _ in range(9)]
 for i in range(9):
     for j in range(9):
         gr[i].append(Entry(takein, width=3, font=("Arial", 30), justify="center"))
-        gr[i][j].grid(column=i, row=j)
+        gr[i][j].grid(column=j, row=i)
 
 
 submit = Button(window, text='Submit Grid', font=('Arial Bold', 15), fg='green', command=fillgrid)
